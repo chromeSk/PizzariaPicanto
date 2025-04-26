90b5b97b17f919bb87a72a342a8c2d971f4ab556
createSK <126958410+chromeSk@users.noreply.github.com>
2025-04-26 13:54:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ async function getProducts() {
     return products;
 };
 
+function getCookieValue(cookieName) {
+    // Розділяємо всі куки на окремі частини
+    const cookies = document.cookie.split(';');
+
+    // Шукаємо куки з вказаним ім'ям
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim(); // Видаляємо зайві пробіли
+
+        // Перевіряємо, чи починається поточне кукі з шуканого імені
+        if (cookie.startsWith(cookieName + '=')) {
+            // Якщо так, повертаємо значення кукі
+            return cookie.substring(cookieName.length + 1); // +1 для пропуску символу "="
+        }
+    }
+    // Якщо кукі з вказаним іменем не знайдено, повертаємо порожній рядок або можна повернути null
+    return '';
+}
+
+
 function getCardHTML(product) {
 
 let productData = JSON.stringify(product);
@@ -135,3 +154,30 @@ function addToCart(event) {
     cart.addItem(product);
     console.log(cart);
 }
+
+let themeBtn = document.querySelector("#themeToggle")
+
+function setTheme(theme) {
+    if (theme == 'light') {
+        document.body.classList.add("light-theme");
+        themeBtn.innerHTML = '<i class="bi bi-moon"></i>';
+    } else {
+        document.body.classList.remove("light-theme");
+        themeBtn.innerHTML = '<i class="bi bi-brightness-high"></i>';
+    }
+}
+
+let theme = getCookieValue('theme')
+setTheme(theme)
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle('light-theme'); // Перемикаємо клас теми
+    if (theme == 'light') {
+        theme = 'dark'
+    } else {
+        theme = 'light'
+    }
+    setTheme(theme)
+    // Зберігаємо JSON рядок у кукі
+    document.cookie = `theme=${theme}; max-age=${60 * 60 * 24 * 7}; path=/`;
+}) 
